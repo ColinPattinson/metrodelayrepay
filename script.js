5562acaf1c479a4e93cc55275e5518c91825f984
Colin Pattinson <colin.pattinson@kry.se>
2023-07-19 00:02:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+/* script.js */
+
+const departures = [
+  {
+    id: 1,
+    destination: "Newcastle",
+    departure_time: "09:00"
+  },
+  {
+    id: 2,
+    destination: "Gateshead",
+    departure_time: "09:30"
+  },
+  {
+    id: 3,
+    destination: "Sunderland",
+    departure_time: "10:00"
+  }
+];
+
+const renderDepartures = () => {
+  const ul = document.querySelector("ul");
+  departures.forEach((departure) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const button = document.createElement("button");
+
+    a.href = `https://www.nexus.org.uk/metro/timetables-and-stations/south-shields/timetable?departure=${departure.id}`;
+    a.textContent = departure.destination;
+
+    button.textContent = "GET REFUND";
+
+    li.appendChild(a);
+    li.appendChild(button);
+
+    ul.appendChild(li);
+  });
+};
+
+renderDepartures();
