06a6b8d16ff4468edc943b421073e08f7277122b
Colin Pattinson <colin.pattinson@kry.se>
2023-07-19 00:08:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* script.js */
-
 const departures = [
   {
     id: 1,
@@ -37,4 +35,32 @@ const renderDepartures = () => {
   });
 };
 
-renderDepartures();
+window.onload = renderDepartures;
+
+// This code adds the ability to filter the departures by destination
+const filterDepartures = (destination) => {
+  const filteredDepartures = departures.filter((departure) => {
+    return departure.destination === destination;
+  });
+
+  const ul = document.querySelector("ul");
+  ul.innerHTML = "";
+
+  filteredDepartures.forEach((departure) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.href = `https://www.nexus.org.uk/metro/timetables-and-stations/south-shields/timetable?departure=${departure.id}`;
+    a.textContent = departure.destination;
+
+    li.appendChild(a);
+
+    ul.appendChild(li);
+  });
+};
+
+const destinationInput = document.querySelector("input");
+destinationInput.addEventListener("input", (event) => {
+  const destination = event.target.value;
+  filterDepartures(destination);
+});
